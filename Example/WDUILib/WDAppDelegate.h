9f8f11d60e07d4d89b861fef54e268adbd6ce9f7
PureWindSN <1983457078@qq.com>
2023-01-02 00:04:45 +0800

    fix error

---
+++ Example/WDUILib/WDAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WDAppDelegate.h
+//  WDUILib
+//
+//  Created by PureWindSN on 12/24/2022.
+//  Copyright (c) 2022 PureWindSN. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/WDUILib/WDViewController.h
@@ -0,0 +1,13 @@
+//
+//  WDViewController.h
+//  WDUILib
+//
+//  Created by PureWindSN on 12/24/2022.
+//  Copyright (c) 2022 PureWindSN. All rights reserved.
+//
+
+@import UIKit;
+
+@interface WDViewController : UIViewController
+
+@end
