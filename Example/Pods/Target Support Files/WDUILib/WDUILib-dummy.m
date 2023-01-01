73b732bfde97e8b14fddfaa9cdee2d0409d2926e
PureWindSN <1983457078@qq.com>
2023-01-02 00:04:45 +0800

    fix error

---
+++ Example/Pods/Target Support Files/WDUILib/WDUILib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WDUILib : NSObject
+@end
+@implementation PodsDummy_WDUILib
+@end
---
+++ Example/WDUILib/WDViewController.m
@@ -0,0 +1,29 @@
+//
+//  WDViewController.m
+//  WDUILib
+//
+//  Created by PureWindSN on 12/24/2022.
+//  Copyright (c) 2022 PureWindSN. All rights reserved.
+//
+
+#import "WDViewController.h"
+
+@interface WDViewController ()
+
+@end
+
+@implementation WDViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
